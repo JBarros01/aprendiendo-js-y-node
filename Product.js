db09f9501d968f81fe67c23d74d1b4655ba0fb81
Jorge Barros <108762331+JBarros01@users.noreply.github.com>
2022-07-19 16:54:38 +0000

    Product class added

---
+++ Product.js
@@ -0,0 +1,6 @@
+export class Product {
+    constructor(product_name, price) {
+        this.product = product_name;
+        this.price = price;
+    }
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import {User} from './User.js'
+import {Product} from './Product.js'
 
 const jorge = new User('Jorge', 'Barros')
 console.log(jorge)
+
+console.log(new Product('laptop', 1400))
